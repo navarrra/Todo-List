b9457a31f4ea76ac0ed4eb6cecc2836bbb289956
Ryan Navarro <ryannavarro@ymail.com>
2018-09-19 18:33:13 -0700

    added delete function

--- src/components/createtkt.js
+++ src/components/createtkt.js
@@ -6,20 +6,45 @@ constructor(props){
   super(props)
   this.state = {
     condition:true,
-    data:["take out trash", "feed dog"]
+    data:[],
+    textvalue : ""
   }
+  this.handleNew = this.handleNew.bind(this)
+  this.handleChange = this.handleChange.bind(this)
+  this.createTkt = this.createTkt.bind(this)
+  this.delete = this.delete.bind(this)
 }
 
 createTkt = () =>{
   this.setState({
-      condition : !this.state.condition
+    condition : !this.state.condition
   })
 };
 
-handleChange(event){
+handleNew(e){
   this.setState({
-    data:event.target.value
-  });
+    textvalue:e.target.value,
+
+  })
+}
+
+handleChange(){
+  this.state.data.push(this.state.textvalue)
+
+  this.setState({
+    condition : !this.state.condition
+  })
+}
+
+delete(i){
+  for(let v = 0; v<this.state.data.length; v++){
+    if(this.state.data[v]===i){
+      delete this.state.data[v]
+    }
+  };
+  this.setState({
+    value: this.state.data
+  })
 }
 
 
@@ -28,15 +53,13 @@ handleChange(event){
   render() {
     let slide= this.state.condition ? "slideout slidein" : "slideout";
     let hide = this.state.condition ? "hide" : "";
-
     return(
     <div>
 
       <div className="menu">
         <div className="controls">
-          <button className="button"><img src={ require("./icons/plus-button.png") } alt="Create" onClick={this.createTkt.bind(this)} className="create"/></button>
-          <button className="button"><img src={ require("./icons/new-task.png") } alt="Edit" onClick={this.edit} className="create"/></button>
-          <button className="button"><img src={ require("./icons/trash.png") } alt="delete" onClick={this.delete} className="create"/></button>
+          <button className="button"><img src={ require("./icons/plus-button.png") } alt="Create" onClick={this.createTkt} className="create"/></button>
+
         </div>
       </div>
 
@@ -45,19 +68,15 @@ handleChange(event){
         <h2 className="newtask">New Task</h2>
 
         <div className="boxes">
-          <input value={this.state.name} className = "name" placeholder="Franchisee name" />
-
-          <input value={this.state.number} placeholder="number" />
-
-          <input value={this.state.task} className="taskinput" placeholder="EnterTask" />
+          <input onChange={this.handleNew} className="taskinput" placeholder="EnterTask" />
 
         </div>
-          <button className="submit" onClick={this.handleChange.bind(this)}>Create</button>
+          <button className="submit" onClick={this.handleChange}>Create</button>
       </div>
       </div>
       <div className="canvas">
       {this.state.data.map((i) =>{
-        return <div className="tkt"><h2 className="todo">{i}</h2><button>Done</button></div>
+        return <div className="tkt"><h2 className="todo">{i}</h2><button onClick={this.delete.bind(this, i)}>done</button></div>
       })}
       </div>
     </div>
